75c18528666b688215d79b4853ccf6de5232a287
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-22 19:45:03 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,12 @@
 class SessionsController < ApplicationController
-    def create
-        user = User.find_by(username: params[:username])
-        if user
-          session[:user_id] = user.id
-          render json: user, status: :ok
-        else
-          render json: { error: 'Invalid username' }, status: :unprocessable_entity
-        end
-      end
+  def create
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id
+    render json: user
+  end
 
-      def destroy
-        session.delete(:user_id)
-        head :no_content
-      end
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
-def show 
-    user = User.find_by(id: session[:user_id])
-    if user
-        render json: user, status:ok
-    else
-        render json:{error: "User not found"},status: :not_found
-    end 
-end
+    def show
+        user = User.find(session[:user_id])
+        render json: user
+      end
 
 end
